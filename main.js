f37d59252a9ac4cee51fe25c7fdeed041d3821af
yuna gando <gando@grean.tech>
2022-09-09 12:16:19 +0900

    衝突判定まで完了

--- main.js
+++ main.js
@@ -8,13 +8,113 @@ const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+const random = (min, max) => {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
 }
 
 // function to generate random color
 
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+const randomRGB = () => {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// ここから
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
+
+// thisはBall?
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
+
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    // this.x = this.x + this.velX
+    this.x += this.velX;
+    // this.y = this.y + this.velY
+    this.y += this.velY;
+}
+
+
+// 衝突判定
+Ball.prototype.collisionDetect = function () {
+    for (let j = 0; J < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const by = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+}
+
+
+const balls = [];
+
+while (balls.length < 25) {
+    let size = random(10, 20);
+    // ボールの位置は、常に少なくともボール 1 個分の幅で描画されます。
+    // 描画エラーを避けるため、キャンバスの端から離して描画されます。
+    let ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        'rgb(' + random(0, 255) + ',' + random(0, 255) + '+' + random(0, 255) + ')',
+        size
+    );
+
+    balls.push(ball);
+}
+
+const loop = () => {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+
