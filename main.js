1d1fbcb9bbfbecb631befa68b93f3957da6b8a43
yuna gando <gando@grean.tech>
2022-09-09 13:58:53 +0900

    完了

--- main.js
+++ main.js
@@ -21,6 +21,16 @@ const randomRGB = () => {
 
 // ここから
 
+/**
+ * ボールオブジェクト
+ *
+ * @param {number} x - 初期表示の x 座標
+ * @param {number} y - 初期表示の y 座標
+ * @param {number} velX - x 方向への移動距離
+ * @param {number} velY - y 方向への移動距離
+ * @param {string} color - ボールの色（'rgb(red, green, blue)'）
+ * @param {number} size - ボールの
+ */
 function Ball(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
@@ -71,10 +81,12 @@ Ball.prototype.update = function () {
 
 // 衝突判定
 Ball.prototype.collisionDetect = function () {
-    for (let j = 0; J < balls.length; j++) {
+    for (let j = 0; j < balls.length; j++) {
+        // ballsがthis(どこのこと?Ball?)と同等ではない場合（!==じゃだめ？）
+        // if (this !== balls[j])
         if (!(this === balls[j])) {
             const dx = this.x - balls[j].x;
-            const by = this.y - balls[j].y;
+            const dy = this.y - balls[j].y;
             const distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + balls[j].size) {
@@ -87,7 +99,7 @@ Ball.prototype.collisionDetect = function () {
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 5) {
     let size = random(10, 20);
     // ボールの位置は、常に少なくともボール 1 個分の幅で描画されます。
     // 描画エラーを避けるため、キャンバスの端から離して描画されます。
@@ -110,6 +122,7 @@ const loop = () => {
     for (let i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect()
     }
 
     requestAnimationFrame(loop);
